301ee7232719e788e7c385d409d82cb0e887077a
harikasailakshmi <97426068+harikasailakshmi@users.noreply.github.com>
2022-10-09 21:02:48 +0530

    import math def nod(a):     c=0     while a>0:         r=a%10         c+=1         a//=10     return c a=int(input()) n=nod(a) asq=a*a if asq%math.pow(10,n)==a:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,23 +1,15 @@
-x=int(input())
-temp=x
-z=x*x
-c=0
-while x>0:
-    d=x%10
-    x=x//10
-    c+=1
-v=0
-w=0
-while z>0 and c>0:
-    d=z%10
-    z=z//10
-    c-=1
-    v=v*10+d
-while v!=0:
-    d=v%10
-    v=v//10
-    w=w*10+d
-if(w==temp):
+import math
+def nod(a):
+    c=0
+    while a>0:
+        r=a%10
+        c+=1
+        a//=10
+    return c
+a=int(input())
+n=nod(a)
+asq=a*a
+if asq%math.pow(10,n)==a:
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
