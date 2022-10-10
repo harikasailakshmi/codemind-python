845a69f884de48ac76160a51894423fed0aeb956
harikasailakshmi <97426068+harikasailakshmi@users.noreply.github.com>
2022-10-10 07:47:49 +0530

    a=input() f=0 le=len(a) for i in range(le):     if (a[i]==a[le-i-1] or a[i]==a[le-i-1].upper() or a[i]==a[le-i-1].lower()):         f=1     else:         f=0         break if f==1:     print("True") else:     print("False")

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,7 +1,13 @@
-x=input()
-x=x.lower()
-k=x[::-1]
-if(k==x):
+a=input()
+f=0
+le=len(a)
+for i in range(le):
+    if (a[i]==a[le-i-1] or a[i]==a[le-i-1].upper() or a[i]==a[le-i-1].lower()):
+        f=1
+    else:
+        f=0
+        break
+if f==1:
     print("True")
 else:
     print("False")
